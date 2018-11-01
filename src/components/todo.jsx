421db9da7950f5c4c63835d60ef82ed3d5775460
Shahan Ahmed Khan <shahanahmed86@github.com>
2018-11-01 21:53:23 +0500

    updating project

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-// import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import * as firebase from 'firebase';
+import './config';
 
 class TodoApp extends Component {
     constructor(props) {
@@ -10,19 +13,67 @@ class TodoApp extends Component {
             message: '',
             editing: false,
             todo: [],
+            key: '',
             uid: props.location.state,
         }
+        this.ref = firebase.database().ref();
     }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData = () => {
+        const { todo, uid } = this.state;
+        this.ref.child(uid).on('value', snapshot => {
+            const obj = snapshot.val();
+            for (let key in obj) {
+                obj[key].key = key;
+                todo.push(obj[key]);
+            }
+            this.setState({ todo })
+        })
+    }
+
     handleChange = ev => {
         const { name, value } = ev.target;
         this.setState({
             [name]: value,
         })
     }
+
+    onSaveHandler = () => {
+        const { message, uid, todo, key, editing } = this.state;
+        console.log(todo);
+        if (message) {
+            if (!editing) {
+                const key = this.ref.child(uid).push().key;
+                this.ref.child(uid).child(key).set({
+                    message,
+                    key,
+                });
+            }
+            else {
+                this.ref.child(uid).child(key).set({
+                    message, key
+                });
+            }
+            this.setState({
+                message: '',
+                key: '',
+                editing: false,
+            });
+        }
+        else {
+            alert('Field can not be left empty');
+        }
+    }
+
     render() {
-        const { message, editing } = this.state;
+        const { message, editing, todo } = this.state;
+        const { classes } = this.props;
         return (
-            <div>
+            <div className={classes.container}>
                 <div>
                     <TextField
                         label='Todo'
@@ -30,11 +81,13 @@ class TodoApp extends Component {
                         name='message'
                         value={message}
                         onChange={this.handleChange}
-                        variant='filled' />
+                        variant='standard' />
                     <Button
+                        className={classes.alignBox}
                         color='secondary'
                         size='small'
-                        variant='contained' >
+                        variant='contained'
+                        onClick={this.onSaveHandler} >
                         {editing ? 'Update' : 'Add'}
                     </Button>
                 </div>
@@ -43,4 +96,20 @@ class TodoApp extends Component {
     }
 }
 
-export default TodoApp;
+const styles = () => ({
+    container: {
+        width: '87%',
+        margin: 'auto',
+        overflow: 'hidden',
+        marginTop: 10,
+    },
+    alignBox: {
+        margin: '15px 5px',
+    }
+});
+
+TodoApp.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(TodoApp);
